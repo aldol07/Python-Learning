ddb296a56870d74dd467b530352894935beb39d6
dubeykartikay1 <149271169+dubeykartikay1@users.noreply.github.com>
2024-08-29 20:10:59 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+tuple1 = (12,3,4,56,'Jenny','Satyam')
+print(type(tuple1))
+tuple2 = (4,4,98,78,567)
+#print(type(tuple2))
+#tuple1[2] = 'zram'
+#tuple3 = tuple1 + tuple2
+#print(len(tuple3))
+list1 = [1,2,3,4]
+#print(min(tuple2)  )
+#print(tuple2.count(4))
+#print(tuple(list1))
+
+
